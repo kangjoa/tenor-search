355f6903561a8c60cb3455681c1038caad1922e9
kangjoa <joannalaikang@gmail.com>
2024-03-20 17:15:49 -0700

    styled page

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const Tenor = require('tenorjs').client({
 // App Setup
 const app = express();
 
+app.use(express.static('public'));
+
 // Middleware
 // Allow Express (our web framework) to render HTML templates and send them back to the client using a new function
 const handlebars = require('express-handlebars');
